168ac4735577bd339fad1ee75a0be2bd995a3006
Macri-man <admfav2012@gmail.com>
2018-10-11 14:01:17 +0100

    changes

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public Texture2D map;
+
+	void GenerateLevel(){
+		for (int i = 0; i < map.width; i++){
+            for (int j = 0; j < map.height; j++)
+            {
+				GenerateTile(i,j);
+            }
+		}
+	}
+
+	void GenerateTile(int xcoord,int ycoord){
+
+	}
+}
