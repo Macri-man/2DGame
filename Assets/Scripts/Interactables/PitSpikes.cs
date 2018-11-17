c85923854442851ef287b0c439872462ea4309c5
PaMat1000 <5500891+PaMat1000@users.noreply.github.com>
2018-11-17 20:14:55 +0000

    20181117-2010
    PM- Updated sprite sheet (added test prompt signs, gravestones, switch parts, new background elements,) changed both sets of layers (new Background4,layer to accommodate new tile layer hopefully won't cause problems), spikes with death trigger, new death audio set up to work with Audio Manager for spikes. Still need to fix climbing, that's next.

---
+++ Assets/Scripts/Interactables/PitSpikes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitSpikes : MonoBehaviour {
+
+	public SoundTrigger HitSound;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		switch(other.gameObject.tag)
+		{
+			case "Player":
+				other.gameObject.GetComponent<playermovement>().death();
+				if(null != HitSound)
+				{
+					HitSound.PlaySound();
+				}
+			break;
+			default:
+								//HitSound.PlaySound();
+								//Destroy(this.gameObject);
+			break;
+
+		}
+	}
+}
