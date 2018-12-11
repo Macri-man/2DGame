3b17631b31dc3410f9e5fadbce50e43cf144fb81
PaMat1000 <5500891+PaMat1000@users.noreply.github.com>
2018-12-11 02:26:11 +0000

    PM-20181211-0225
    How to play screen incorporated, link to How to play added to main menu, updates to FinalLevel

---
+++ Assets/Scripts/Menus/HowToPlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HowToPlay : MonoBehaviour {
+
+
+	public void PlayGame(){
+		SceneManager.LoadScene("Level1");
+	}
+	public void BackToMenu(){
+		SceneManager.LoadScene("Menu");
+	}
+	public void QuitGame(){
+		Debug.Log("QUITING");
+		Application.Quit();
+	}
+
+}
--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -5,9 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	
+
 	public void PlayGame(){
 		SceneManager.LoadScene("Level1");
+	}
+	public void HowToPlay(){
+		SceneManager.LoadScene("HowToPlay");
 	}
 
 	public void QuitGame(){
